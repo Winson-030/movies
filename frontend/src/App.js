d51f5d3c84695fb28a3f25f623102448495dc864
Winson Lee <a623719265@gmail.com>
2023-07-13 02:04:32 +0800

    upload source code

---
+++ frontend/src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+import {useEffect, useState} from "react";
+import api from './api/axiosConfig';
+import Layout from "./layout/Layout";
+import {Route, Routes} from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Trailer from "./components/tailer/Tailer";
+import Reviews from "./components/reviews/Reviews";
+import NotFound from "./components/notFount/NotFound";
+
+function App() {
+    const [movies, setMovies] = useState();
+    const [reviews, setReviews] = useState([]);
+    const [movie, setMovie] = useState();
+
+    const getMovies = async () => {
+        try {
+            const response = await api.get("/api/v1/movies");
+            console.log(response.data)
+            setMovies(response.data);
+            console.log(response.headers);
+        } catch (e) {
+            console.log(e);
+        }
+
+    }
+    const getMovieData = async (movieId) => {
+
+        try {
+            const response = await api.get(`/api/v1/movies/${movieId}`);
+            const singleMovie = response.data;
+            setMovie(singleMovie);
+            setReviews(singleMovie.reviews);
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+    useEffect(() => {
+        getMovies();
+    }, [])
+
+    return (
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route path="/" element={<Home movies={movies}/>}></Route>
+                    <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+                    <Route path="/Reviews/:movieId"
+                           element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews}
+                                             setReviews={setReviews}/>}></Route>
+                    <Route path="*" element={<NotFound/>}></Route>
+                </Route>
+            </Routes>
+
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/components/notFount/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <>
+            <h3>This page is not found!</h3>
+        </>
+    )
+}
+
+export default NotFound;
